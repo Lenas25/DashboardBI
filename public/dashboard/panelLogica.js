c28c71928454c5532cbcc78b1d57a45babeaec83
Lenas25 <102402197+Lenas25@users.noreply.github.com>
2025-02-20 14:24:32 -0500

    finish to push dashboards

--- public/dashboard/panelLogica.js
+++ public/dashboard/panelLogica.js
@@ -1,16 +1,18 @@
 const dashboards = [
   {
-    title: "Inventario y Logística",
+    title: "Dashboard de ventas y facturación",
+    frame:
+      '<iframe title="Dashboard de Ventas" width="1140" height="541.25"  src="https://app.powerbi.com/view?r=eyJrIjoiZGQ4MjNiOTgtMDZlNC00YTViLTgxNWUtNzE0OGUyYTgwZTAxIiwidCI6ImM0YTY2YzM0LTJiYjctNDUxZi04YmUxLWIyYzI2YTQzMDE1OCIsImMiOjR9" frameborder="0" allowFullScreen="true" style="border: none; width: 100%; height: 100%;"></iframe>',
+  },
+  {
+    title: "Dashboard de inventario y logística",
     frame:
       '<iframe title="Dashboard de Inventario" width="1140" height="541.25" src="https://app.powerbi.com/view?r=eyJrIjoiZmI4Yjg3NmEtNGJhNi00MDBhLTlhN2MtOGNjNmNlYzljYjdiIiwidCI6ImM0YTY2YzM0LTJiYjctNDUxZi04YmUxLWIyYzI2YTQzMDE1OCIsImMiOjR9" frameborder="0" allowFullScreen="true" style="border: none; width: 100%; height: 100%;"></iframe>',
   },
   {
-    title: "",
-    frame: "",
-  },
-  {
-    title: "",
-    frame: "",
+    title: "Dashboard de comportamiento del cliente",
+    frame:
+      '<iframe title="Dashboard de Comentarios" width="1140" height="541.25" src="https://app.powerbi.com/view?r=eyJrIjoiNjAwZjBjZDAtYThlNS00MmI1LTgyMzQtYzM0ZDIxNzkzOThkIiwidCI6ImM0YTY2YzM0LTJiYjctNDUxZi04YmUxLWIyYzI2YTQzMDE1OCIsImMiOjR9" frameborder="0" allowFullScreen="true" style="border: none; width: 100%; height: 100%;"></iframe>',
   },
 ];
 
@@ -44,17 +46,41 @@ const titulo = document.getElementById("tituloDashboard");
 
 const updateTitle = (text) => {
   titulo.innerText = text;
+  const notificationInventory = document.getElementById("notification-block");
+  notificationInventory.innerHTML = "";
 };
 
 const changeDashboard = (title) => {
+  if (title === "Dashboard de inventario y logística") {
+    const notificationInventory = document.getElementById("notification-block");
+    notificationInventory.innerHTML = `<div class="notification-item" id="bajoStock">
+                  <div class="notification-title">
+                    <p>Productos Bajos en Stock</p>
+                  </div>
+                  <ul id="productosBajoStock">
+                  </ul>
+                </div>
+                <div class="notification-item" id="prontoVencimiento">
+                  <div class="notification-title">
+                    <p>Productos Pronto a Vencer</p>
+                  </div>
+                  <ul id="productosProntoVencer">
+                  </ul>
+                </div>`;
+    obtenerBajoStock();
+    obtenerProntoVencimiento();
+  }
+
   const dashboardFrame = document.getElementById("dashboard");
   dashboardFrame.innerHTML = "";
+  console.log("Frame:", dashboardFrame);
+  console.log("Title:", title);
   const dashboard = dashboards.find((d) =>
-    title.toLowerCase().includes(d.title.toLowerCase())
+    d.title.toLowerCase().includes(title.toLowerCase())
   );
-  if (dashboard) {
-    dashboardFrame.innerHTML = dashboard.frame;
-  }
+  console.log("Dashboard found:", dashboard);
+  dashboardFrame.innerHTML = dashboard.frame;
+  console.log("Dashboard frame set:", dashboard.frame);
 };
 
 const convertirCSVInventario = async () => {
@@ -66,6 +92,7 @@ const convertirCSVInventario = async () => {
     const data = await response.text();
     const filas = data.split("\n").map((row) => row.split(","));
     const encabezados = filas.shift();
+    encabezados[10] = "fechavencimiento";
     const inventario = filas.map((fila) => {
       const objeto = {};
       fila.forEach((valor, index) => {
@@ -88,7 +115,7 @@ const obtenerBajoStock = async () => {
     const listProductos = document.getElementById("productosBajoStock");
     for (const producto of notificaciones) {
       const li = document.createElement("li");
-      li.innerText = `${producto.producto} - Stock: ${producto.stockactual}`;
+      li.innerText = `* ${producto.producto} - Stock: ${producto.stockactual}`;
       listProductos.appendChild(li);
     }
   } catch (error) {
@@ -99,19 +126,29 @@ const obtenerBajoStock = async () => {
 const obtenerProntoVencimiento = async () => {
   try {
     const inventario = await convertirCSVInventario();
-    const notificaciones = inventario.filter(
-      (item) => {
-        const fechaVencimiento = new Date(item.fechavencimiento);
-        const hoy = new Date();
-        const diferencia = fechaVencimiento - hoy;
-        return diferencia / (1000 * 60 * 60 * 24) <= 30;
-      }
-    );  
+    const notificaciones = inventario.filter((item) => {
+      const fechaVencimiento = new Date(item.fechavencimiento);
+      const hoy = new Date();
+      const diferencia = fechaVencimiento - hoy;
+      const diasRestantes = diferencia / (1000 * 60 * 60 * 24);
+      return diasRestantes <= 30;
+    });
     const listProductos = document.getElementById("productosProntoVencer");
     for (const producto of notificaciones) {
       const li = document.createElement("li");
-      const diasvencimiento = new Date(producto.fechavencimiento) - new Date();
-      li.innerText = `${producto.producto} - Vence en: ${diasvencimiento} días`;
+      let diasRestantes = Math.ceil(
+        (new Date(producto.fechavencimiento) - new Date()) /
+          (1000 * 60 * 60 * 24)
+      );
+      if (diasRestantes === 0) {
+        li.innerText = `* ${producto.producto} - Vence hoy`;
+      } else if (diasRestantes <= 0) {
+        diasRestantes *= -1;
+        li.innerText = `* ${producto.producto} - Venció hace ${diasRestantes} días`;
+      } else {
+        li.innerText = `* ${producto.producto} - Vence en ${diasRestantes} días`;
+      }
+
       listProductos.appendChild(li);
     }
   } catch (error) {
@@ -143,8 +180,8 @@ const cerrarSesion = () => {
 document.addEventListener("DOMContentLoaded", () => {
   checkExistence();
   handleResize();
-  obtenerBajoStock();
-  obtenerProntoVencimiento();
+  const dashboardframe = document.getElementById("dashboard");
+  dashboardframe.innerHTML = dashboards[0].frame;
   const items = document.querySelectorAll("aside .item");
   items.forEach((item, index) => {
     if (index < 3) {
