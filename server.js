88624d3fb2cab0d6d406e5dfae3589f6e700d9a8
Lenas25 <102402197+Lenas25@users.noreply.github.com>
2025-02-25 16:56:28 -0500

    changes

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+const port = process.env.PORT || 3000;
+
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/inicio', (req, res) => {
@@ -27,6 +29,6 @@ app.get('/api/users', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('Servidor iniciado en http://localhost:3000');
 });
